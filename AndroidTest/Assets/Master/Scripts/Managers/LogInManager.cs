f5b024b1b4c39f2fc239b9a8f99adee9fa4ff10a
Riku935 <exodo302.richy@gmail.com>
2023-05-03 16:52:37 -0600

    Listo

---
+++ AndroidTest/Assets/Master/Scripts/Managers/LogInManager.cs
@@ -0,0 +1,56 @@
+using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
+
+public class LogInManager : MonoBehaviour
+{
+    //    private PlayGamesClientConfiguration clientConfiguration;
+    //    /* public Text statusTxt;
+    //     public Text descriptionTxt;*/
+
+    //    private void Start()
+    //    {
+    //        ConfiguredGPGS();
+    //        SignIntoGPGS(SignInInteractivity.CanPromptOnce, clientConfiguration);
+    //    }
+
+    //    internal void ConfiguredGPGS()
+    //    {
+    //        clientConfiguration = new PlayGamesClientConfiguration.Builder().Build();
+    //    }
+
+    //    internal void SignIntoGPGS(SignInInteractivity interactivity, PlayGamesClientConfiguration configuration)
+    //    {
+    //        configuration = clientConfiguration;
+    //        PlayGamesPlatform.InitializeInstance(configuration);
+    //        PlayGamesPlatform.Activate();
+    //        PlayGamesPlatform.Instance.Authenticate(interactivity, (code) =>
+    //        {
+    //            // statusTxt.text = "Authenticating";
+    //            if (code == SignInStatus.Success)
+    //            {
+    //                Debug.Log("Yes");
+    //            }
+    //            else
+    //            {
+    //                Debug.Log("Yesnt");
+    //            }
+    //        });
+    //    }
+
+    //    // Referencias:
+    //    // - public void BasicsignInBtn()
+    //    //   SignIntoGPGS(SignInInteractivity.CanPromptAlways, clientConfiguration);
+    //    // - public void SignoutBtn()
+    //    //   PlayGamesPlatform.Instance.SignOut();
+    //    //   statusTxt.text = "Signed Out";
+    //    //   descriptionTxt.text = "";
+    //}
+}
+
