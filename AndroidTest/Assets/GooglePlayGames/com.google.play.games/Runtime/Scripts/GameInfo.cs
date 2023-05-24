d096160c0cec46b29ea372b7bb76a5444a976010
Riku935 <exodo302.richy@gmail.com>
2023-05-23 20:34:50 -0600

    Listo sign in

--- AndroidTest/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ AndroidTest/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -38,7 +38,7 @@ namespace GooglePlayGames {
 
         public const string ApplicationId = "715113300718"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "715113300718-7bhb68qu33ue34j8dhj64pqaa0dv9lqj.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
